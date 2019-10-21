a960bc9b534fe6e1d3d8874916b7f61a0a2f9c0d
Jaikangam Malangmei <jaikangam12@gmail.com>
2019-10-21 11:24:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 3000;
+
+//Mongo Db Connection
+const mongojs = require('mongojs');
+const db = mongojs('clientkeeper', ['clients']);
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+  res.send('Please use /api/clients');
+});
+
+app.listen(port, () => {
+  console.log('Server started on port' +port);
+});
